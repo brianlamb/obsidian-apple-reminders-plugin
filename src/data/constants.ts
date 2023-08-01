3f954f5173a85f5b7bcacf8e094135594ed6a042
Emir Habul <emir@tillion.io>
2023-08-01 14:53:51 +0200

    Fix issue with date format

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,1 +1,4 @@
-export const APPLE_DATE_FORMAT = "dddd, DD MMMM at H:m:ss";
+// Original format
+// export const APPLE_DATE_FORMAT = "dddd, DD MMMM at H:m:ss";
+// but this depends on the locale and possibly other things, setting it for myself
+export const APPLE_DATE_FORMAT = "dddd, MMMM DD, YYYY at H:mm:ss";
